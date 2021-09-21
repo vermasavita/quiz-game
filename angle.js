909610f70e028ebd314077c3236c78ab070b7c46
Savita Verma <svitaverma10@gmail.com>
2021-09-21 12:56:12 +0530

    fix: added condition to take only positive value

--- angle.js
+++ angle.js
@@ -10,11 +10,20 @@ function calculateSumOfAngles(angle1, angle2, angle3) {
 };
 
 function isTriangle() {
-    var sumOfAngles = calculateSumOfAngles(Number(angleInputs[0].value), Number(angleInputs[1].value), Number(angleInputs[2].value));
-        if (sumOfAngles === 180) {
-            output.innerText = "You are right! The angle does form a triangle"
+    if (Number(angleInputs[0].value) && Number(angleInputs[1].value) && Number(angleInputs[2].value)) {
+        if (Number(angleInputs[0].value) > 0 && Number(angleInputs[1].value) > 0 && Number(angleInputs[2].value) >0) {
+            var sumOfAngles = calculateSumOfAngles(Number(angleInputs[0].value), Number(angleInputs[1].value), Number(angleInputs[2].value));
+            if (sumOfAngles === 180) {
+                output.innerText = "You are right! The angle does form a triangle"
+            } else {
+                output.innerText = "Oh ho! you are wrong! The angle doesn't form a triangle"
+            }
         } else {
-            output.innerText = "Oh ho! you are wrong! The angle doesn't form a triangle"
-        }
-    
+            output.innerText = "Enter valid number"
+        } 
+    }else{
+        output.innerText = "Empty field"
+    }
+
+
 };
